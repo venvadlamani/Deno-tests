1b05331825583ba5b72efad1d66ffb5138ed6cc1
venvadlamani <122305594+venvadlamani@users.noreply.github.com>
2023-05-21 09:11:19 -0500

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
-import { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
+import { Application, Router } from 'oak';
+import { oakCors } from 'cors';
 
-serve((req: Request) => new Response('Hello World'));
+
+const router = new Router();
+const app = new Application();
+
+app.use(async (ctx, next) => await routeMiddleware(ctx, next));
+
+router
+  .get("/contact", getAllContacts)
+
+  
+app.use(
+  oakCors({ origin: "*", preflightContinue: true }),
+);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
+
+export default app;
